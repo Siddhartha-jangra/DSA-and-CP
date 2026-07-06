376159fe07420d0f8850d22d54d5aa09a3927f4b
Siddhartha-jangra <jangrasidhu1876@gmail.com>
2026-07-06 09:31:53 +0530

    changed 4 files

---
+++ codeforces/2241.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define pb push_back
+
+void solve() {
+    int x,y; cin >> x>> y;
+    if(x%y == 0){
+        cout << "YES\n";
+    }else{
+        cout << "NO\n";
+    }
+
+}
+
+int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t; // Remove or comment out if the problem only has one test case
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
